4bb2b2176e74e1b564d86efa2dad28830ef0c4fa
Behrooz Fili <behrooz.filzadeh@gmail.com>
2026-03-06 13:01:49 +0100

    add data validation pipeline and artifact generation

--- main.py
+++ main.py
@@ -2,7 +2,8 @@ from networksecurity.exception.exception import NetworkSecurityException
 from networksecurity.logging.logger import logging
 import sys
 from networksecurity.components.data_ingestion import DataIngestion
-from networksecurity.entity.config_entity import DataIngestionConfig
+from networksecurity.components.data_validation import DataValidation
+from networksecurity.entity.config_entity import DataIngestionConfig,DataValidationConfig
 from networksecurity.entity.config_entity import TrainingPipelineConfig
 
 
@@ -14,9 +15,14 @@ if __name__=='__main__':
     trainingpiplineconfig=TrainingPipelineConfig()
     dataingestionconfig=DataIngestionConfig(trainingpiplineconfig)
     data_ingestion=DataIngestion(dataingestionconfig)
-    dataingestionartifact=data_ingestion.initiate_data_ingestion()
-    
     logging.info("initiate the data ingestion")
+    dataingestionartifact=data_ingestion.initiate_data_ingestion()
+    logging.info("data ingestion completed")
+    datavalidationconfig=DataValidationConfig(trainingpiplineconfig)
+    data_validation=DataValidation(dataingestionconfig,datavalidationconfig)
+    logging.info("initiate the data validation")
+    datavalidationartifact=data_validation.initiate_data_validation()
+    logging.info("data validation completed")
   except Exception as e:
     raise NetworkSecurityException(e,sys)
   
--- networksecurity/components/data_ingestion.py
+++ networksecurity/components/data_ingestion.py
@@ -62,10 +62,10 @@ class DataIngestion :
         train_set,test_set =train_test_split(dataframe,test_size=self.data_ingestion_config.train_test_split_ratio)
         logging.info("performed train test split on the dataframe")
         logging.info("Exited split_data_as_train_test method of Data_ingestion class")
-        dir_path = os.path.dirname(self.data_ingestion_config.training_file_path)
+        dir_path = os.path.dirname(self.data_ingestion_config.train_file_path)
         os.makedirs(dir_path,exist_ok=True)
         logging.info(f"Exporting train and test file path")
-        train_set.to_csv(self.data_ingestion_config.training_file_path,index=False,header=True)
+        train_set.to_csv(self.data_ingestion_config.train_file_path,index=False,header=True)
         test_set.to_csv(self.data_ingestion_config.test_file_path,index=False,header=True)
         logging.info("Exported train and test file path")
     except Exception as e:
@@ -76,7 +76,7 @@ class DataIngestion :
       dataframe=self.export_collection_as_dataframe()
       dataframe=self.export_data_into_feature_store(dataframe)
       self.split_data_as_train_test(dataframe)
-      data_ingestion_artifact=DataIngestionArtifact(train_file_path=self.data_ingestion_config.training_file_path,
+      data_ingestion_artifact=DataIngestionArtifact(train_file_path=self.data_ingestion_config.train_file_path,
                                                          test_file_path=self.data_ingestion_config.test_file_path)
       return data_ingestion_artifact
     except Exception as e:
